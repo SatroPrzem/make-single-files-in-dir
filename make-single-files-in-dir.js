0099787badb659ebfff6a9e48f5b47d9ad7f2979
SatroPrzem <przemyslaw.satro@gmail.com>
2024-05-30 09:55:46 +0200

    rewrite script with fs.promises

--- make-single-files-in-dir.js
+++ make-single-files-in-dir.js
@@ -1,45 +1,65 @@
 const {
-  mkdirSync,
-  readdirSync,
-  statSync,
-  renameSync,
-  existsSync,
-} = require("fs");
-const { join, parse, extname } = require("path");
+  mkdir,
+  readdir,
+  stat,
+  rename,
+  access,
+} = require("fs").promises;
+const {
+  join,
+  parse,
+  extname
+} = require("path");
 
 const filesDoesNotWillBeReplaced = ['make-single-files-in-dir.js', 'package.json', 'README.md'];
 
- (() => {
-  const EXTNAME = process.argv[2];
-  const WHERE_TO_RUN_SCRIPT = process.argv[3] ? process.argv[3] : "./";
-
-  if (!existsSync(WHERE_TO_RUN_SCRIPT)) {
-    console.error("This path does not exist", WHERE_TO_RUN_SCRIPT);
+(async () => {
+  const WHERE_TO_RUN_SCRIPT = process.argv[2] ? process.argv[2] : "./";
+  const EXTNAME = process.argv[3];
+  
+  try {
+    await access(WHERE_TO_RUN_SCRIPT);
+  }
+  catch {
+    console.error("This path does not exist, or you have no permissions to this path: ", WHERE_TO_RUN_SCRIPT);
     return;
   }
 
-  readdirSync(WHERE_TO_RUN_SCRIPT).forEach((fileName) => {
-    if (filesDoesNotWillBeReplaced.includes(fileName)) {
-      return;
-    }
-    
-    if (statSync(join(WHERE_TO_RUN_SCRIPT, fileName)).isFile()) {
-      if (EXTNAME && extname(fileName) !== EXTNAME) {
-        return;
-      }
-      
-      const folderName = parse(fileName).name;
-      const folderPathWithName = join(WHERE_TO_RUN_SCRIPT, folderName);
-
-      if (!existsSync(folderPathWithName)) {
-        mkdirSync(folderPathWithName);
-        console.log(`directory created: ${folderName}`);
+  try {
+    const files = await readdir(WHERE_TO_RUN_SCRIPT);
+  
+    for (const fileName of files) {
+      if (filesDoesNotWillBeReplaced.includes(fileName)) {
+        continue;
       }
 
-      const oldFilePath = join(WHERE_TO_RUN_SCRIPT, fileName);
-      const newFilePath = join(folderPathWithName, fileName);
-      renameSync(oldFilePath, newFilePath);
-      console.log(`Moved file ${fileName} to directory ${folderName}`);
+      const filePath = join(WHERE_TO_RUN_SCRIPT, fileName);
+      const fileStat = await stat(filePath);
+
+      if (fileStat.isFile()) {
+        if (EXTNAME && extname(fileName) !== EXTNAME) {
+          continue;
+        }
+
+        const folderName = parse(fileName).name;
+        const folderPathWithName = join(WHERE_TO_RUN_SCRIPT, folderName);
+
+        try {
+          await access(folderPathWithName);
+        }
+        catch {
+          await mkdir(folderPathWithName);
+          console.log(`directory created: ${folderName}`);
+        }
+
+        const oldFilePath = filePath;
+        const newFilePath = join(folderPathWithName, fileName);
+        await rename(oldFilePath, newFilePath);
+        console.log(`Moved file ${fileName} to directory ${folderName}`);
+      }
     }
-  });
+  }
+  catch (error) {
+    console.error("Error has accurate:", error);
+  }
 })();
